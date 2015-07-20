04e628f6ac286478ea1c9b9bf4b610327b6ae4f1
j094097 <j094097@gmail.com>
2015-07-20 11:41:04 +0800

    add USER

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ module Nikomica
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = 'zh-TW'
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      "<div class=\"field error\">#{html_tag}</div>".html_safe
+    }
   end
 end
