6d243e72be95b6dc1dfdf6f1a059ee3f2af33462
j094097 <j094097@gmail.com>
2015-07-21 11:32:03 +0800

    add ckeditor css and js

---
+++ public/assets/ckeditor/config-fdb71d5fe81057099fdee0e2ffb6c86b.js
@@ -0,0 +1,11 @@
+CKEDITOR.config.toolbar= [
+  { name: 'styles', items : [ 'Styles','Format' ] },
+  { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+  { name: 'insert', items : [ 'Image', 'Table']},
+  '/',
+  { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+  { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
+  { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+  { name: 'tools', items : [ 'Maximize','-','About' ] }
+]
+;
